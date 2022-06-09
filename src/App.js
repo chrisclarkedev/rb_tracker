94ced58ebaf4e8cb295ceacdbb9cc6914e1d7767
chrisclarkedev <cclarke8904@gmail.com>
2022-06-09 14:34:15 -0400

    imported add form component to App.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Budget from './components/Budget';
 import Remaining from './components/Remaining';
 import Total from './components/Total';
 import List from './components/List';
+import AddForm from './components/AddForm';
 
 const App = () => {
   return (
@@ -24,6 +25,12 @@ const App = () => {
       <div className="row mt-3">
         <div className="col-sm">
           <List />
+        </div>
+      </div>
+      <h3 className="mt-3">Additional Costs</h3>
+      <div className=" mt-3">
+        <div className="col-sm">
+          <AddForm />
         </div>
       </div>
     </div>
