f7e6f820f2c98d6b3ff7f4008fdc06f3c3443d76
chrisclarkedev <cclarke8904@gmail.com>
2022-11-09 11:51:13 -0500

    fixes

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Budget = () => {
   return (
     <div className="alert alert-secondary">
-      <span>Budget: $200</span>
+      <span>Budget: $350</span>
     </div>
   );
 };
