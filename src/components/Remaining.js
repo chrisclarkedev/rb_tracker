4b7a210d2323bd95b15a2d4b0aaf2c3438347454
chrisclarkedev <cclarke8904@gmail.com>
2022-11-16 18:11:50 -0500

    added total functionality that updates on submit

--- src/components/Remaining.js
+++ src/components/Remaining.js
@@ -8,8 +8,11 @@ const Remaining = () => {
     return (total = total + item.cost);
   }, 0);
 
+  // Alert type that will change remaining color if balance goes into the negative
+  const alertType = totalExpenses > budget ? 'alert-danger' : 'alert-success';
+
   return (
-    <div className="alert alert-success">
+    <div className={`alert ${alertType}`}>
       <span>Remaining: ${budget - totalExpenses}</span>
     </div>
   );
--- src/components/Total.js
+++ src/components/Total.js
@@ -1,9 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
 
 const Total = () => {
+  const { expenses } = useContext(AppContext);
+
+  const totalExpenses = expenses.reduce((total, item) => {
+    return (total += item.cost);
+  }, 0);
+
   return (
     <div className="alert alert-primary">
-      <span>Spent: $120</span>
+      <span>Spent: ${totalExpenses}</span>
     </div>
   );
 };
